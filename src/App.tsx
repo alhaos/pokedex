bf27e7e868cfde142b6fee106141bc843a5618ca
ALSemenov <alhaos@gmail.com>
2026-04-26 09:25:59 +0500

    feat: add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useFetch } from "./hooks/useFetch";
 import type { PokemonListResponse } from "./types/pokemon";
 import { PokemonCard } from "./components/PokemonCard";
+import { useState } from "react";
 
 function App() {
+  const [offset, setOffset] = useState(0);
   const { data, loading, error } = useFetch<PokemonListResponse>(
-    "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0",
+    `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`,
   );
 
   return (
@@ -19,6 +21,15 @@ function App() {
           ))}
         </div>
       )}
+
+      <div>
+        <button onClick={() => setOffset(offset - 20)} disabled={offset === 0}>
+          Назад
+        </button>
+        <button onClick={() => setOffset(offset + 20)} disabled={!data?.next}>
+          Вперед
+        </button>
+      </div>
     </div>
   );
 }
